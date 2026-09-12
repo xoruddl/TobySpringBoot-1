f01bd6d90e20da399ca5494f74ce10e7c35b6cce
itaekyung <taeyun1411@gmail.com>
2026-09-12 11:47:49 +0900

    study: 서블릿 등록

--- src/main/java/toby/springboot1/Springboot1Application.java
+++ src/main/java/toby/springboot1/Springboot1Application.java
@@ -1,14 +1,30 @@
 package toby.springboot1;
 
+import jakarta.servlet.ServletContext;
+import jakarta.servlet.ServletException;
+import jakarta.servlet.http.HttpServlet;
+import jakarta.servlet.http.HttpServletRequest;
+import jakarta.servlet.http.HttpServletResponse;
+import java.io.IOException;
 import org.springframework.boot.tomcat.servlet.TomcatServletWebServerFactory;
 import org.springframework.boot.web.server.WebServer;
 import org.springframework.boot.web.server.servlet.ServletWebServerFactory;
+import org.springframework.boot.web.servlet.ServletContextInitializer;
 
 public class Springboot1Application {
 
 	public static void main(String[] args) {
 		ServletWebServerFactory serverFactory = new TomcatServletWebServerFactory();
-		WebServer webServer = serverFactory.getWebServer();
+		WebServer webServer = serverFactory.getWebServer(servletContext -> {
+            servletContext.addServlet("hello", new HttpServlet() {
+				@Override
+				protected void service(HttpServletRequest req, HttpServletResponse resp) throws ServletException, IOException {
+					resp.setStatus(200);
+					resp.setHeader("Content-Type", "text/plain");
+					resp.getWriter().println("Hello Servlet");
+				}
+            }).addMapping("/hello"); 
+        });
 		webServer.start();
 	}
 
