7f67fbe4b9aafad95bdbb2628fb2490bcd50467b
itaekyung <taeyun1411@gmail.com>
2026-09-12 16:45:07 +0900

    study: 스프링 컨테이너로 통합

--- src/main/java/toby/springboot1/Springboot1Application.java
+++ src/main/java/toby/springboot1/Springboot1Application.java
@@ -1,5 +1,6 @@
 package toby.springboot1;
 
+import org.springframework.beans.BeansException;
 import org.springframework.boot.tomcat.servlet.TomcatServletWebServerFactory;
 import org.springframework.boot.web.server.WebServer;
 import org.springframework.boot.web.server.servlet.ServletWebServerFactory;
@@ -9,18 +10,25 @@ import org.springframework.web.servlet.DispatcherServlet;
 public class Springboot1Application {
 
 	public static void main(String[] args) {
-		GenericWebApplicationContext applicationContext = new GenericWebApplicationContext();
+		GenericWebApplicationContext applicationContext = new GenericWebApplicationContext() {
+			@Override
+			protected void onRefresh() throws BeansException {
+				super.onRefresh();
+
+				ServletWebServerFactory serverFactory = new TomcatServletWebServerFactory();
+				WebServer webServer = serverFactory.getWebServer(servletContext -> {
+					servletContext.addServlet("dispatcherServlet",
+							new DispatcherServlet(this)
+					).addMapping("/*");
+				});
+				webServer.start();
+			}
+		};
 		applicationContext.registerBean(HelloController.class);
 		applicationContext.registerBean(SimpleHelloService.class);
 		applicationContext.refresh();
 
-		ServletWebServerFactory serverFactory = new TomcatServletWebServerFactory();
-		WebServer webServer = serverFactory.getWebServer(servletContext -> {
-			servletContext.addServlet("dispatcherServlet",
-						new DispatcherServlet(applicationContext)
-					).addMapping("/*");
-        });
-		webServer.start();
+
 	}
 
 }
