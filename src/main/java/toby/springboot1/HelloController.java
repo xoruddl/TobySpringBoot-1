1497d46ae12b45fdd09743bb3a225d4428e7cfc1
itaekyung <taeyun1411@gmail.com>
2026-09-12 15:03:41 +0900

    study: 스프링 컨테이너 사용

--- src/main/java/toby/springboot1/HelloController.java
+++ src/main/java/toby/springboot1/HelloController.java
@@ -1,8 +1,5 @@
 package toby.springboot1;
 
-import org.springframework.web.bind.annotation.GetMapping;
-import org.springframework.web.bind.annotation.RestController;
-
 public class HelloController {
 
     public String hello(String name) {
--- src/main/java/toby/springboot1/Springboot1Application.java
+++ src/main/java/toby/springboot1/Springboot1Application.java
@@ -8,6 +8,7 @@ import java.io.IOException;
 import org.springframework.boot.tomcat.servlet.TomcatServletWebServerFactory;
 import org.springframework.boot.web.server.WebServer;
 import org.springframework.boot.web.server.servlet.ServletWebServerFactory;
+import org.springframework.context.support.GenericApplicationContext;
 import org.springframework.http.HttpHeaders;
 import org.springframework.http.HttpMethod;
 import org.springframework.http.HttpStatus;
@@ -16,10 +17,12 @@ import org.springframework.http.MediaType;
 public class Springboot1Application {
 
 	public static void main(String[] args) {
+		GenericApplicationContext applicationContext = new GenericApplicationContext();
+		applicationContext.registerBean(HelloController.class);
+		applicationContext.refresh();
+
 		ServletWebServerFactory serverFactory = new TomcatServletWebServerFactory();
 		WebServer webServer = serverFactory.getWebServer(servletContext -> {
-			HelloController helloController = new HelloController();
-
 			servletContext.addServlet("frontcontroller", new HttpServlet() {
 				@Override
 				protected void service(HttpServletRequest req, HttpServletResponse resp) throws ServletException, IOException {
@@ -27,13 +30,11 @@ public class Springboot1Application {
 					if (req.getRequestURI().equals("/hello") && req.getMethod().equals(HttpMethod.GET.name())) {
 						String name = req.getParameter("name");
 
+						HelloController helloController = applicationContext.getBean(HelloController.class);
 						String ret = helloController.hello(name);
 
-						resp.setStatus(HttpStatus.OK.value());
-						resp.setHeader(HttpHeaders.CONTENT_TYPE, MediaType.TEXT_PLAIN_VALUE);
+						resp.setContentType(MediaType.TEXT_PLAIN_VALUE);
 						resp.getWriter().println(ret);
-					} else if (req.getRequestURI().equals("/user")) {
-
 					} else {
 						resp.setStatus(HttpStatus.NOT_FOUND.value());
 					}
