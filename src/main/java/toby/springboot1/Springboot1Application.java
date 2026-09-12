28db06b0c43528123864059ed912e2d32f67b458
itaekyung <taeyun1411@gmail.com>
2026-09-12 17:06:50 +0900

    study: 자바 코드 구성 정보 사용

--- src/main/java/toby/springboot1/Springboot1Application.java
+++ src/main/java/toby/springboot1/Springboot1Application.java
@@ -4,31 +4,41 @@ import org.springframework.beans.BeansException;
 import org.springframework.boot.tomcat.servlet.TomcatServletWebServerFactory;
 import org.springframework.boot.web.server.WebServer;
 import org.springframework.boot.web.server.servlet.ServletWebServerFactory;
+import org.springframework.context.annotation.Bean;
+import org.springframework.context.annotation.Configuration;
+import org.springframework.web.context.support.AnnotationConfigWebApplicationContext;
 import org.springframework.web.context.support.GenericWebApplicationContext;
 import org.springframework.web.servlet.DispatcherServlet;
 
+@Configuration
 public class Springboot1Application {
 
+	@Bean
+	public HelloController helloController(HelloService helloService) {
+		return new HelloController(helloService);
+	}
+
+	@Bean
+	public HelloService helloService() {
+		return new SimpleHelloService();
+	}
+
 	public static void main(String[] args) {
-		GenericWebApplicationContext applicationContext = new GenericWebApplicationContext() {
+		AnnotationConfigWebApplicationContext applicationContext = new AnnotationConfigWebApplicationContext() {
 			@Override
-			protected void onRefresh() throws BeansException {
+			protected void onRefresh(){
 				super.onRefresh();
 
 				ServletWebServerFactory serverFactory = new TomcatServletWebServerFactory();
 				WebServer webServer = serverFactory.getWebServer(servletContext -> {
 					servletContext.addServlet("dispatcherServlet",
-							new DispatcherServlet(this)
-					).addMapping("/*");
+							new DispatcherServlet(this))
+							.addMapping("/*");
 				});
 				webServer.start();
 			}
 		};
-		applicationContext.registerBean(HelloController.class);
-		applicationContext.registerBean(SimpleHelloService.class);
+		applicationContext.register(Springboot1Application.class);
 		applicationContext.refresh();
-
-
 	}
-
 }
