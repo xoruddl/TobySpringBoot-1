ea2ae54788f5e778e2da255ea684219f45e7e1ac
itaekyung <taeyun1411@gmail.com>
2026-09-21 11:56:35 +0900

    study: 메타 애노테이션과 합성 애노테이션

--- src/test/java/toby/springboot1/HelloServiceTest.java
+++ src/test/java/toby/springboot1/HelloServiceTest.java
@@ -1,11 +1,27 @@
 package toby.springboot1;
 
+import java.lang.annotation.ElementType;
+import java.lang.annotation.Retention;
+import java.lang.annotation.RetentionPolicy;
+import java.lang.annotation.Target;
 import org.assertj.core.api.Assertions;
 import org.junit.jupiter.api.Test;
 
+@Retention(RetentionPolicy.RUNTIME)
+@Target(ElementType.METHOD)
+@UnitTest
+@interface FastUnitTest {
+}
+
+@Retention(RetentionPolicy.RUNTIME)
+@Target({ElementType.ANNOTATION_TYPE, ElementType.METHOD})
+@Test
+@interface UnitTest {
+}
+
 public class HelloServiceTest {
 
-    @Test
+    @FastUnitTest
     void SimpleHelloService() {
         SimpleHelloService helloService = new SimpleHelloService();
 
