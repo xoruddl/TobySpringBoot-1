76219fc4e50978fe81670c0b3d0e815d5133daa4
itaekyung <taeyun1411@gmail.com>
2026-09-21 12:08:32 +0900

    study: 합성 애노테이션의 적용

--- src/main/java/toby/springboot1/Config.java
+++ src/main/java/toby/springboot1/Config.java
@@ -1,18 +1,21 @@
 package toby.springboot1;
 
-import org.springframework.boot.SpringApplication;
 import org.springframework.boot.tomcat.servlet.TomcatServletWebServerFactory;
 import org.springframework.boot.web.server.servlet.ServletWebServerFactory;
 import org.springframework.context.annotation.Bean;
-import org.springframework.context.annotation.ComponentScan;
 import org.springframework.context.annotation.Configuration;
 import org.springframework.web.servlet.DispatcherServlet;
 
-@MySpringBootAnnotation
-public class Springboot1Application {
+@Configuration
+public class Config {
 
-	public static void main(String[] args) {
-//		MySpringApplication.run(Springboot1Application.class, args);
-		SpringApplication.run(Springboot1Application.class, args);
-	}
+    @Bean
+    public ServletWebServerFactory servletWebServerFactory() {
+        return new TomcatServletWebServerFactory();
+    }
+
+    @Bean
+    public DispatcherServlet dispatcherServlet() {
+        return new DispatcherServlet();
+    }
 }
